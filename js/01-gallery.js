50b7f2c06ef920b2bfc20b105001e211e0043e60
Maria Yeromina <mariayeromina@Marias-MacBook-Air.local>
2023-11-01 16:02:41 +0100

    task8 validation

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryItems = document.querySelector(".galerry-item");
+const galleryLink = document.querySelector(".gallery-link");
+const galerryImage = document.querySelector(".gallery-image");
+
+
+
+
 
 console.log(galleryItems);
