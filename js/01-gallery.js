ba443fa6a6282f6d5c2c3ec0c08c5f126918dd53
Maria Yeromina <mariayeromina@Marias-MacBook-Air.local>
2023-11-05 16:56:11 +0100

    home lesson 2: task 1 and task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector(".gallery");
-const galleryItems = document.querySelector(".galerry-item");
-const galleryLink = document.querySelector(".gallery-link");
-const galerryImage = document.querySelector(".gallery-image");
-
-
-
-
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+const galleryItemEl = galleryItems.map((galleryItemCard) => {
+    return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItemCard.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItemCard.preview}"
+      data-source="${galleryItemCard.original}"
+      alt="${galleryItemCard.description}"
+    />
+  </a>
+</li>`;
+});
+gallery.insertAdjacentHTML("beforeend", galleryItemEl.join(""));
+
+let instance;
+const onGalleryItemClick = (event) => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const originalPath = event.target.dataset.source;
+    console.log(originalPath);
+
+    const basicLightboxOption = {
+        onClose() {
+            document.removeEventListener("keydown", onDocumentKeyPress);
+        },
+    };
+    instance = basicLightbox.create(
+        `<img src="${originalPath}" width="800" height="600">`, 
+        basicLightboxOption
+    );
+    instance.show();
+
+    document.addEventListener("keydown", onDocumentKeyPress);
+};
+const onDocumentKeyPress = (event) => {
+    if (event.code === "Escape") {
+        instance.close();
+    }
+};
+gallery.addEventListener("click", onGalleryItemClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector("ul.gallery");
+
+function createMarkUp({ preview, original, description }) {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+};
+const galleryCartArray = galleryItems.map(item => createMarkUp(item));
+console.log(galleryCartArray);
+
+galleryList.innerHTML = galleryCartArray.join("");
+
+new SimpleLightbox(".gallery a", { captionDate: "alt", captionDelay: 250 });
